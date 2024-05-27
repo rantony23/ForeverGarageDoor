ea319c0c9838061cdf1a00e50e5db157b80709be
Reinaldo Cuevas <rantony52@gmail.com>
2024-05-27 18:22:42 -0500

    se agrego la ultima funcionalidad en js para la pagina inicial tiene todo , falta pulir detalles de medidas y texto

--- js/index.js
+++ js/index.js
@@ -48,6 +48,11 @@ const images = document.querySelectorAll('.slider-button');
 
 btnleft.addEventListener("click",e => moveToLeft());
 btnright.addEventListener("click",e => moveToRight());
+
+setInterval(()=>{
+    moveToRight()
+},5000);
+
 let operacion=0;
 let widthImg = 300/images.length;
 let counter =0;
@@ -57,7 +62,7 @@ function moveToRight(){
         operacion = 0;
         counter=0;
         slider.style.transform = `translate(-${operacion}%)`;
-        slider.style.transition = "all ease .6s"
+        slider.style.transition = "none"
     }else{
         counter++;
         operacion = operacion + widthImg;
@@ -66,9 +71,18 @@ function moveToRight(){
     };
 }
 function moveToLeft(){
-    operacion = operacion- widthImg;
-    slider.style.transform = `translate(-${operacion}%)`;
-    slider.style.transition="all ease .6s";
+    counter--;
+    if(counter < 0){
+        counter = images.length-1;
+        operacion = widthImg*(images.length);
+        console.log(operacion);
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition = "none";
+    }else{
+        operacion = operacion- widthImg;
+        slider.style.transform = `translate(-${operacion}%)`;
+        slider.style.transition="all ease .6s";
+    }
 }
 
 // SLIDER IMG
@@ -80,3 +94,68 @@ window.addEventListener('load', function() {
         document.querySelector('.sliding-div').classList.add('active');
     }, 3000); // 3000 ms = 3 segundos
 });
+
+// LOGOS
+let logosCompanies = document.getElementById('logos-companies');
+let prevScreenSizeIsMobile = window.innerWidth <= 768;
+
+function checkScreenSize() {
+    if (window.innerWidth <= 768) {
+        logosCompanies.classList.add('ocultar');
+        prevScreenSizeIsMobile= true;
+    } else {
+        if(prevScreenSizeIsMobile){
+            reset();
+        }
+        logosCompanies.classList.remove('ocultar');
+        prevScreenSizeIsMobile =false;
+    }
+}
+
+window.addEventListener('resize', checkScreenSize);
+checkScreenSize();
+
+
+
+
+const deslizar = document.getElementById('logos-companies');
+const logos = document.querySelectorAll('.slider-div');
+
+
+let oper=0;
+let widthLogos = 100/logos.length;
+let contador =0;
+console.log(widthLogos);
+function moveToDerecha(){
+    if(contador >=3){
+        oper = 0;
+        contador=0;
+        deslizar.style.transform = `translate(-${oper}%)`;
+        deslizar.style.transition = "none"
+    }else{
+        contador++;
+        oper = oper + widthLogos;
+        deslizar.style.transform = `translate(-${oper}%)`;
+        deslizar.style.transition= "all ease .6s";
+    };
+}
+function addClickEvent() {
+    const right = document.querySelector('.ocultar');
+    if (right) {
+        right.removeEventListener("click", moveToDerecha);
+        right.addEventListener("click",moveToDerecha);
+    }
+}
+addClickEvent();
+
+setInterval(() => {
+    if (logosCompanies.classList.contains('ocultar')) {
+        moveToDerecha();
+    }
+}, 2000);
+function reset() {
+    oper = 0;
+    contador = 0;
+    deslizar.style.transform = `translate(0%)`;
+    deslizar.style.transition = 'none';
+}
